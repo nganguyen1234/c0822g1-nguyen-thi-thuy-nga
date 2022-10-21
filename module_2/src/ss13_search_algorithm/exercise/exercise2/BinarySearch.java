0fb85273b8457dcf7a16d8540bab0df9dc04cdac
nganguyen1234 <ngathuy3456@gmail.com>
2022-10-21 16:29:27 +0700

    update

---
+++ module_2/src/ss13_search_algorithm/exercise/exercise2/BinarySearch.java
@@ -0,0 +1,34 @@
+package ss13_search_algorithm.exercise.exercise2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhạp vào số lương phần tử của mảng:");
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhập vào giá trị phần tử thứ " + (i + 1));
+            arr[i] = Integer.parseInt(scanner.nextLine());
+        }
+        Arrays.sort(arr);
+        System.out.println(binarySearch(arr, 0, arr.length - 1, 2));
+    }
+
+    public static int binarySearch(int[] array, int left, int right, int value) {
+        if (right >= left) {
+            int middle = (left + right) / 2;
+            if (array[middle] == value) {
+                return middle;
+            }
+            if (value > array[middle]) {
+                return binarySearch(array, middle + 1, right, value);
+            }
+            return binarySearch(array, left, middle - 1, value);
+        }
+        return -1;
+    }
+}
+

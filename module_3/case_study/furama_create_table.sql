4f1efb634152042d825137dd48f890cb542c0fc7
nganguyen1234 <ngathuy3456@gmail.com>
2022-11-14 16:50:05 +0700

    update

--- module_3/case_study/furama_create_table.sql
+++ module_3/case_study/furama_create_table.sql
@@ -1,1 +1,109 @@
-create database furama_create_table;
+create database if not exists furama_create_table;
+use furama_create_table;
+-- tạo bảng vi_tri--
+create table vi_tri(
+  ma_vi_tri int not null primary key, 
+  ten_vi_tri varchar(45)
+);
+-- tạo bảng trinh_do--
+create table trinh_do(
+  ma_trinh_do int not null primary key, 
+  ten_trinh_do varchar(45)
+);
+-- tạo bảng bo_phan--
+create table bo_phan(
+  ma_bo_phan int not null primary key, 
+  ten_bo_phan varchar(45)
+);
+-- tạo bảng nhan_vien--
+create table nhan_vien(
+  ma_nhan_vien int not null primary key, 
+  ho_ten varchar(45), 
+  ngay_sinh date, 
+  so_cmnd varchar(45), 
+  luong double, 
+  so_dien_thoai varchar(45), 
+  email varchar(45), 
+  dia_chi varchar(45), 
+  ma_vi_tri int, 
+  foreign key(ma_vi_tri) references vi_tri(ma_vi_tri), 
+  ma_trinh_do int, 
+  foreign key(ma_trinh_do) references trinh_do(ma_trinh_do), 
+  ma_bo_phan int,
+  foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
+);
+-- tạo bảng loai_khach--
+create table loai_khach(
+  ma_loai_khach int not null primary key, 
+  ten_loai_khach varchar(45)
+);
+-- tạo bảng khach_hang--
+create table khach_hang(
+  ma_khach_hang int primary key, 
+  ma_loai_khach int, 
+  foreign key (ma_loai_khach) references loai_khach(ma_loai_khach), 
+  ho_ten varchar(45), 
+  ngay_sinh date, 
+  gioi_tinh bit(1), 
+  so_cmnd varchar(45), 
+  so_dien_thoai varchar(45), 
+  email varchar(45), 
+  dia_chi varchar(45)
+);
+-- tạo bảng loai_dich_vu --
+create table loai_dich_vu(
+  ma_loai_dich_vu int not null primary key, 
+  ten_loai_dich_vu varchar(45)
+);
+-- tạo bảng kieu_thue--
+create table kieu_thue(
+  ma_kieu_thue int not null primary key, 
+  ten_kieu_thue varchar(45)
+);
+-- tạo bảng dich_vu--
+create table dich_vu(
+  ma_dich_vu int not null primary key, 
+  ten_dich_vu varchar(45), 
+  dien_tich int, 
+  chi_phi_thue double, 
+  so_nguoi_toi_da int, 
+  ma_kieu_thue int,
+  foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue), 
+  ma_loai_dich_vu int,
+  foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu), 
+  tieu_chuan_phong varchar(45), 
+  mo_ta_tien_nghi_khac varchar(45), 
+  dien_tich_ho_boi double, 
+  so_tang int, 
+  dich_vu_mien_phi_di_kem text
+);
+-- tạo bảng hop_dong--
+create table hop_dong(
+  ma_hop_dong int not null primary key, 
+  ngay_lam_hop_dong datetime, 
+  ngay_ket_thuc datetime, 
+  tien_dat_coc double, 
+  ma_nhan_vien int,
+  foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien), 
+  ma_khach_hang int,
+ foreign key(ma_khach_hang) references khach_hang(ma_khach_hang), 
+  ma_dich_vu int,
+ foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
+);
+-- tạo bảng dich_vu_di_kem--
+create table dich_vu_di_kem(
+  ma_dich_vu_di_kem int not null primary key, 
+  ten_dich_vu_di_kem varchar(45), 
+  gia double, 
+  don_vi varchar(10), 
+  trang_thai varchar(45)
+);
+-- tạo bảng hop_dong_chi_tiet--
+create table hop_dong_chi_tiet(
+  ma_hop_dong_chi_tiet int not null primary key, 
+  ma_hop_dong int,
+  foreign key(ma_hop_dong) references hop_dong(ma_hop_dong), 
+  ma_dich_vu_di_kem int,
+ foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem), 
+  so_luong int
+);
---
+++ module_3/case_study/furama_insert.sql
@@ -0,0 +1,20 @@
+use furama_create_table;
+insert into vi_tri values
+(1,"Quản Lý"),
+(2,"Nhân Viên");
+
+insert into trinh_do values
+(1,"Trung Cấp"),
+(2,"Cao Đẳng"),
+(3,"Đại Học"),
+(4,"Sau Đại Học");
+
+insert into bo_phan values
+(1,"Sale-Marketing"),
+(2,"Hành chính"),
+(3,"Phục vụ"),
+(4,"Quản lý");
+
+insert into nhan_vien value
+(1,"Nguyễn Văn An",1970-11-07,456231786,10000000,0901234121,"annguyen@gmail.com","295 Nguyễn Tất Thành","Đà Nẵng",1,3,1);
+select * from bo_phan;

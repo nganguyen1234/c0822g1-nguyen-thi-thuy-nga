09ee0a1c8a8fcf043ec39968e78d5aba20d43a9e
nganguyen1234 <ngathuy3456@gmail.com>
2022-11-15 16:22:10 +0700

    update

--- module_3/ss2_erd/exercise/exercise1.sql
+++ module_3/ss2_erd/exercise/exercise1.sql
@@ -1,65 +1,60 @@
 create database if not exists erd_demo;
 use erd_demo;
 create table phieu_xuat(
-so_px int auto_increment primary key,
-ngay_xuat date);
-
+  so_px int auto_increment primary key, 
+  ngay_xuat date
+);
 create table vat_tu(
-ma_vtu int auto_increment primary key,
-ten_vtu varchar(50)
+  ma_vtu int auto_increment primary key, 
+  ten_vtu varchar(50)
 );
-
 create table chi_tiet_phieu_xuat(
-so_px int,
-ma_vtu int,
-dg_xuat double,
-sl_xuat int,
-primary key(so_px,ma_vtu),
-foreign key(so_px) references phieu_xuat(so_px),
-foreign key(ma_vtu) references vat_tu(ma_vtu)
+  so_px int, 
+  ma_vtu int, 
+  dg_xuat double, 
+  sl_xuat int, 
+  primary key(so_px, ma_vtu), 
+  foreign key(so_px) references phieu_xuat(so_px), 
+  foreign key(ma_vtu) references vat_tu(ma_vtu)
 );
-
 create table phieu_nhap(
-so_pn int auto_increment primary key,
-ngay_nhap date
+  so_pn int auto_increment primary key, 
+  ngay_nhap date
 );
-
 create table chi_tiet_phieu_nhap(
-ma_vtu int,
-so_pn int,
-dg_nhap double,
-sl_nhap int,
-primary key(ma_vtu,so_pn),
-foreign key(so_pn) references phieu_nhap(so_pn),
-foreign key(ma_vtu) references vat_tu(ma_vtu)
+  ma_vtu int, 
+  so_pn int, 
+  dg_nhap double, 
+  sl_nhap int, 
+  primary key(ma_vtu, so_pn), 
+  foreign key(so_pn) references phieu_nhap(so_pn), 
+  foreign key(ma_vtu) references vat_tu(ma_vtu)
 );
-
 create table nha_cc(
-ma_ncc int auto_increment primary key,
-ten_ncc varchar(50),
-dia_chi varchar(50)
+  ma_ncc int auto_increment primary key, 
+  ten_ncc varchar(50), 
+  dia_chi varchar(50)
 );
 create table sdt(
-sdt int primary key,
-ma_ncc int,
-foreign key(ma_ncc) references nha_cc(ma_ncc)
+  sdt int primary key, 
+  ma_ncc int, 
+  foreign key(ma_ncc) references nha_cc(ma_ncc)
 );
 create table don_dh(
-so_dh int auto_increment primary key,
-ngay_dh date
+  so_dh int auto_increment primary key, 
+  ngay_dh date
 );
 create table cung_cap(
-ma_ncc int,
-so_dh int,
-primary key(ma_ncc,so_dh),
-foreign key(ma_ncc) references nha_cc(ma_ncc),
-foreign key(so_dh) references don_dh(so_dh)
+  ma_ncc int, 
+  so_dh int, 
+  primary key(ma_ncc, so_dh), 
+  foreign key(ma_ncc) references nha_cc(ma_ncc), 
+  foreign key(so_dh) references don_dh(so_dh)
 );
 create table chi_tiet_don_dh(
-ma_vtu int,
-so_dh int,
-primary key(ma_vtu,so_dh),
-foreign key(ma_vtu) references vat_tu(ma_vtu),
-foreign key(so_dh) references don_dh(so_dh)
+  ma_vtu int, 
+  so_dh int, 
+  primary key(ma_vtu, so_dh), 
+  foreign key(ma_vtu) references vat_tu(ma_vtu), 
+  foreign key(so_dh) references don_dh(so_dh)
 );
-
--- module_3/ss2_erd/exercise/exercise2.sql
+++ module_3/ss2_erd/exercise/exercise2.sql
@@ -1,29 +1,27 @@
 create database if not exists store_management;
 use store_management;
 create table customer(
-customer_id int auto_increment primary key,
-customer_name varchar(50),
-customer_age int
+  customer_id int auto_increment primary key, 
+  customer_name varchar(50), 
+  customer_age int
 );
 create table product(
-product_id int auto_increment primary key,
-product_name varchar(50),
-product_price double
+  product_id int auto_increment primary key, 
+  product_name varchar(50), 
+  product_price double
 );
-
 create table `order`(
-order_id int auto_increment primary key,
-customer_id int,
-foreign key(customer_id) references customer(customer_id),
-order_date date,
-order_total_price double
+  order_id int auto_increment primary key, 
+  customer_id int, 
+  foreign key(customer_id) references customer(customer_id), 
+  order_date date, 
+  order_total_price double
 );
-
 create table order_detail(
-order_id int,
-product_id int,
-primary key(order_id,product_id),
-foreign key(order_id) references `order`(order_id),
-foreign key(product_id) references product(product_id),
-order_qty int
+  order_id int, 
+  product_id int, 
+  primary key(order_id, product_id), 
+  foreign key(order_id) references `order`(order_id), 
+  foreign key(product_id) references product(product_id), 
+  order_qty int
 );

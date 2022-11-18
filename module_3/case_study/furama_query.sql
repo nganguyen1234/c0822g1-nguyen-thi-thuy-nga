65e1bc7ec9fd7270c12f55456ae3729bf7b58d34
nganguyen1234 <ngathuy3456@gmail.com>
2022-11-18 22:55:27 +0700

    update

--- module_3/case_study/furama_query.sql
+++ module_3/case_study/furama_query.sql
@@ -91,3 +91,13 @@ group by ho_ten;
 -- nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
  select *, count(distinct ma_khach_hang) from hop_dong where year(ngay_lam_hop_dong) = 2021 
  group by month(ngay_lam_hop_dong);
+ 
+ -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+ -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem
+ -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+ select hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, ifnull(sum(hdct.so_luong),0) as so_luong_dich_vu_di_kem 
+ from hop_dong as hd
+ left join hop_dong_chi_tiet as hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+ group by hd.ma_hop_dong;
+ 
+ 
